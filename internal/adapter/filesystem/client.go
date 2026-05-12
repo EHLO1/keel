8202a7033fa088d57db19d73243024040538859d
EHLO <joseph.ley@outlook.com>
2026-05-12 16:55:11 -0500

    Added some rough methods

--- internal/adapter/filesystem/client.go
+++ internal/adapter/filesystem/client.go
@@ -1,7 +1,9 @@
 package filesystem
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 )
@@ -10,8 +12,10 @@ type FilesystemClient struct {
 	baseDir string
 }
 
+// make a map of paths to initialize the clients!
+
 func NewClient(baseDir string) (*FilesystemClient, error) {
-	// Clean the path and ensure the directory exists with correct permissions
+	// Clean the provided baseDir, create if it doesn't exist, fix permissions.
 	cleanDir := filepath.Clean(baseDir)
 	if err := os.MkdirAll(cleanDir, 0644); err != nil {
 		return nil, fmt.Errorf("failed to create base directory %s: %w", cleanDir, err)
@@ -22,6 +26,67 @@ func NewClient(baseDir string) (*FilesystemClient, error) {
 	}, nil
 }
 
-func (c *FilesystemClient) Write() {
+func (c *FilesystemClient) SetMaintenanceMode(mm bool) error {
+	mfe, err := maintFileExists(c)
+	if err != nil {
+		return err
+	}
 
+	switch {
+	case !mm && !mfe:
+		return fmt.Errorf("maintenance mode is not enabled: %s", err)
+
+	case !mm && mfe:
+		_, err := os.Create(c.baseDir + "/maintenance_mode")
+		if err != nil {
+			return fmt.Errorf("failed to enable maintenance mode: %s", err)
+		}
+
+	case mm && mfe:
+		return fmt.Errorf("maintenance mode is already enabled: %s", err)
+
+	case mm && !mfe:
+		err := os.Remove(c.baseDir + "/maintenance_mode")
+		if err != nil {
+			return fmt.Errorf("failed to disable maintenance mode: %s", err)
+		}
+
+	default:
+		panic(fmt.Errorf("maintenance mode unknown: %s", err))
+	}
+	return nil
+}
+
+func maintFileExists(c *FilesystemClient) (bool, error) {
+	_, err := os.Stat(c.baseDir + "/maintenance_mode")
+	if !errors.Is(err, fs.ErrNotExist) {
+		return true, nil
+	}
+	return false, err
+}
+
+func (c *FilesystemClient) GetStandbySignal() (bool, error) {
+	sse, err := standbySignalExists(c)
+	if err != nil {
+		return false, err
+	}
+
+	switch {
+	case sse:
+		return true, nil
+
+	case !sse:
+		return false, nil
+
+	default:
+		panic(fmt.Errorf("standby signal status unknown: %s", err))
+	}
+}
+
+func standbySignalExists(dv string) (bool, error) {
+	_, err := os.Stat(dv + "/maintenance_mode")
+	if !errors.Is(err, fs.ErrNotExist) {
+		return true, nil
+	}
+	return false, err
 }
---
+++ internal/adapter/filesystem/types.go
@@ -0,0 +1,16 @@
+package filesystem
+
+type MaintenanceFlag struct {
+	path string
+}
+
+type VRRPRole struct {
+	path string
+}
+
+type StandbySignal struct {
+	path string
+}
+
+type Client interface {
+}
